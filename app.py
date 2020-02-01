43fbe73c74f9fdb93d02ddbbf6c3fc59a17d8fb0
Hershill <47647238+Hershill@users.noreply.github.com>
2020-02-01 00:31:06 -0500

    Add Dockerfile

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 
@@ -8,4 +10,4 @@ def studyQ():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
