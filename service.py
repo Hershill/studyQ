c0f15f74d4394722640a260d38ce9f2202a43b44
Matthieu Roux <matthieurouxleoncini@gmail.com>
2020-02-01 11:52:18 -0500

    updated my magnificient test function

--- service.py
+++ service.py
@@ -1,6 +1,30 @@
 def get_sample_quiz():
     quiz = {
-        "name": "hehe"
+        "id": "2b811da9-57f1-43db-8d67-9f9dc1c7958a",
+        "name": "hehe",
+        "questions": [
+            {
+                "id": "bee4acb6-5d64-48fe-b6e6-0856c9826462",
+                "question": "Which number is cooler",
+                "answers": [
+                    {"answer": "420", "isCorrect": False},
+                    {"answer": "666", "isCorrect": False},
+                    {"answer": "42", "isCorrect": False},
+                    {"answer": "69", "isCorrect": True}
+                ]
+            },
+            {
+                "id": "21529fa9-dfe6-4ba8-97d7-f57eda22e830",
+                "question": "What's the average air speed of a swallow",
+                "answers": [
+                    {"answer": "20 m/s", "isCorrect": False},
+                    {"answer": "40 m/s", "isCorrect": False},
+                    {"answer": "11 m/s", "isCorrect": True},
+                    {"answer": "69 m/s", "isCorrect": False}
+                ]
+            }
+        ],
+        "score": 0
     }
 
     return quiz
