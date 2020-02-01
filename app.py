84fe64a53d78a6759e6964998947e0469a47d583
Hershil <15hd16@queensu.ca>
2020-02-01 10:08:35 -0500

    add sample data

--- app.py
+++ app.py
@@ -14,5 +14,25 @@ def studyQnew():
     return 'It works!'
 
 
+@app.route('/studyQ/get_quizzes')
+def studyq_get_quizzes():
+    """
+    Returns quizzes objects associated with account id
+
+    :return:
+    """
+    return 'It works!'
+
+
+@app.route('/studyQ/get_quizz')
+def studyq_get_quizzes():
+    """
+    Map quiz objects to account and return quizzes
+
+    :return:
+    """
+    return 'It works!'
+
+
 if __name__ == '__main__':
     app.run(host='localhost', port=int(os.environ.get('PORT', 8080)))
