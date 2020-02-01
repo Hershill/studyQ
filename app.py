9e7fd67718cd6b66f150d0d79ff0d6c2b0ac265e
Hershil <15hd16@queensu.ca>
2020-02-01 10:49:19 -0500

    Add flask config

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
 import os
+import logging
 
 app = Flask(__name__)
+
+# Logger
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+                              datefmt='%Y-%m-%d %H:%M:%S')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+# Load the configuration from the config file
+app.config.from_pyfile('config.py')
 
 
 @app.route('/')
@@ -22,16 +35,20 @@ def studyq_get_quizzes():
     :return:
     """
 
+    # Get account ID
+
     return 'It works!'
 
 
-@app.route('/studyQ/get_quiz')
+@app.route('/studyQ/get_quiz', methods=['GET', 'POST'])
 def studyq_get_quiz():
     """
     Map quiz objects to account and return quizzes
 
     :return:
     """
+    if request.method == 'GET':
+        return make_response(jsonify("Hello"), 200)
     return 'It works!'
 
 
---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+import logging
+
+# Using FLASK_ENV to get the current environment and adjust the configuration.
+app_env = os.environ.get('FLASK_ENV')
+
+# If FLASK_ENV is not set, flask defaults to production, we want to set to development as default so during local
+if app_env is None:
+    os.environ['FLASK_ENV'] = 'development'
+    app_env = os.environ.get('FLASK_ENV')
+
+
+# Settings applied to specific environments
+if app_env == 'production':
+    DEBUG = False
+
+elif app_env == 'development':
+    DEBUG = True
+
+
+# required to keep each user session secure and use flasks session variables
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+
+# Display configuration that was set in the console
+logging.info("Config Env: {}".format(app_env))
