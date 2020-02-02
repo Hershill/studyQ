fc3d185f06abc31a0c01f5d3ac2ed2d64d0f5041
Matthieu Roux <matthieurouxleoncini@gmail.com>
2020-02-02 06:30:51 -0500

    Add post option

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def root():
     return jsonify(list(sample)[0])
 
 
-@app.route('/studyQ/quiz', methods=['GET'])
+@app.route('/studyQ/quiz', methods=['GET', 'POST'])
 def create_quiz():
     username = request.values.get("username")
     quiz = request.values.get("quiz")
